3099c0b0ddd0d054a3fbeb867636b2bc18d8093a
László Huszár <160109237+LaszloHuszar@users.noreply.github.com>
2024-06-26 12:15:22 +0200

    express.js basics and projects

--- projects/backend/Express Server/index.js
+++ projects/backend/Express Server/index.js
@@ -2,6 +2,22 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+})
+
+app.post("/register", (req, res) => {
+  res.sendStatus(201);
+})
+
+app.put("/usr/laszlo", (req, res) => {
+  res.sendStatus(200);
+})
+
+app.patch("/usr/laszlo", (req, res) => {
+  res.sendStatus(200);
+})
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
